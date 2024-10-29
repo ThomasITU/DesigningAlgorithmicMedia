31db6b11830605bda8d121f33fd0c0c15da0fa04
ThomasITU <thwr@berkeley.edu>
2024-10-29 03:20:44 -0700

    Update git ignore to avoid add temporary conditions to contain coloring based on feature functionality

--- src/analysis.py
+++ src/analysis.py
@@ -24,21 +24,24 @@ class Analysis:
 
 
     @staticmethod   
-    def plot_PCA(pca_df, coloring_feature, dataset_name):
+    def plot_PCA(pca_df, dataset_name, coloring_feature):
 
-        pca_df[coloring_feature[1]] = coloring_feature[0]  # Add the coloring feature
-        # Normalize the E033 values for color mapping
-        norm = plt.Normalize(coloring_feature[0].min(), coloring_feature[0].max())
-        cmap = plt.get_cmap('coolwarm')  # Get the colormap from blue to red
-
-        # Map E033 values to colors
-        colors = cmap(norm(coloring_feature[0]))
-
-        # Create a scatter plot
         plt.figure(figsize=(10, 6))
-        scatter = plt.scatter(x=pca_df['PC1'], y=pca_df['PC2'], c=colors, alpha=0.7)
 
-        plt.title(f'PCA Plot of {dataset_name} Colored by {coloring_feature[1]} ({coloring_feature[2]})')
+        if coloring_feature is not None:
+            pca_df[coloring_feature[1]] = coloring_feature[0]  # Add the coloring feature
+            # Normalize the E033 values for color mapping
+            norm = plt.Normalize(coloring_feature[0].min(), coloring_feature[0].max())
+            cmap = plt.get_cmap('coolwarm')  # Get the colormap from blue to red
+
+            # Map E033 values to colors
+            colors = cmap(norm(coloring_feature[0]))
+            scatter = plt.scatter(x=pca_df['PC1'], y=pca_df['PC2'],alpha=0.7, c=colors)
+            plt.title(f'PCA Plot of {dataset_name} Colored by {coloring_feature[1]} ({coloring_feature[2]})')
+
+        else:
+            scatter = plt.scatter(x=pca_df['PC1'], y=pca_df['PC2'],alpha=0.7)
+        plt.title(f'PCA Plot of {dataset_name}')
         plt.xlabel('Principal Component 1')
         plt.ylabel('Principal Component 2')
         plt.grid()
